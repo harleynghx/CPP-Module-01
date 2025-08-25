aec1ef2365194f3e32f6859899b82b0e180bda82
harleynghx <harleynghx.edu@gmail.com>
2025-08-25 16:04:02 +0800

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // The string variable
+    std::string str = "HI THIS IS BRAIN";
+    
+    // stringPTR: A pointer to the string
+    std::string* stringPTR = &str;
+    
+    // stringREF: A reference to the string
+    std::string& stringREF = str;
+
+    
+    // Print memory addresses
+    std::cout << "Memory addresses:" << std::endl;
+    std::cout << "• Address of string variable: " << &str << std::endl;
+    std::cout << "• Address held by stringPTR:  " << stringPTR << std::endl;
+    std::cout << "• Address held by stringREF:  " << &stringREF << std::endl;
+    
+    std::cout << std::endl;
+    
+    // Print values
+    std::cout << "Values:" << std::endl;
+    std::cout << "• Value of string variable:  " << str << std::endl;
+    std::cout << "• Value pointed by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "• Value pointed by stringREF: " << stringREF << std::endl;
+    
+    return 0;
+}
